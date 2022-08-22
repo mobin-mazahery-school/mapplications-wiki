93644ac7dad0c05b3f6aeb197d9c2286d89f962f
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 20:24:52 +0430

    Commit

--- api/index.py
+++ api/index.py
@@ -8,18 +8,7 @@ app = Flask(__name__)
 @app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
 def main(path):
-    print(path)
-    if os.path.isfile(f"static/{path}.html"):
-        tmpd = open(f"static/{path}.html", "r", encoding="utf-8").read()
-        if "<htmltags:" in tmpd:
-            for _, match in enumerate(re.finditer(r"<htmltags:(.+?)\/>", tmpd, re.MULTILINE), start=1):
-                fname = match.groups()[0]
-                if os.path.isfile(f"html-tags/{fname}.html"):
-                    tmpd = tmpd.replace(match.group(), open(f"html-tags/{fname}.html", "r", encoding="utf-8").read())
-                    print(match.group())
-        return tmpd
-    else:
-        return "Not found"
+    return send_from_directory("static", f"{path}.html")
 
 if __name__ == "__main__":
     app.run()
