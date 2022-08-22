c89c1a7126abc5a30646102b35626bca86810dfd
mobin mazahery <98708517+mobin-mazahery-school@users.noreply.github.com>
2022-08-22 18:50:55 +0430

    Commit

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route("/", defaults={"path": ""}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
 def main(path):
+    print(path)
     if len(path) <= 0:
         path = "index.html"
     if os.path.isfile(path):
