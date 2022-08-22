fa4b3dee95ba39b09f3b5d33d5e803ee951c755b
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 19:57:24 +0430

    commit

--- api/index.py
+++ api/index.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 import os
+import re
 
 app = Flask(__name__)
 
 
-@app.route("/", defaults={"path": ""}, methods=["GET"])
+@app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
 def main(path):
-    print(path)
-    if len(path) <= 0:
-        path = "index.html"
-    if os.path.isfile(path):
-        return open(f"static/{path}","r").read()
+    if os.path.isfile(f"static/{path}.html"):
+        tmpd = open(f"static/{path}.html").read()
+        if "<html-tags:" in tmpd:
+            for _, match in enumerate(re.finditer(r"<html-tags:(.+?)\/>", tmpd, re.MULTILINE), start=1):
+                fname = match.groups()[0]
+                if os.path.isfile(f"html-tags/{fname}"):
+                    tmpd = tmpd.replace(match.group(), open(f"html-tags/{fname}").read())
+        return tmpd
+
+if __name__ == "__main__":
+    app.run()
