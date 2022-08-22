6fef678b4cb109d4fe83eca5840893b4c74f34d6
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 20:40:19 +0430

    Commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect
 import os
 import re
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ app = Flask(__name__)
 @app.route("/<path:path>", methods=["GET"])
 def main(path):
     return send_from_directory("static", f"{path}.html")
+
+@app.route("/download/MInstagramBot")
+def download(app):
+    if not app == "":
+        ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content
+        accesscode = requests.get("https://m-applications.cf/gak.php").content
+        return redirect(f"https://m-applications.cf/download.php?version={ver}&k={accesscode}")
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run()
