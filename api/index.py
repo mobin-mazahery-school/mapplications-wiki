28a72e1506b3e3468ec9d9db355640ee20269562
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 23:04:04 +0430

    One Step until Finish

--- api/index.py
+++ api/index.py
@@ -1,13 +1,14 @@
 from flask import Flask, send_from_directory, redirect
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route("/download/MInstagramBot")
 def download():
     ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content.decode("utf-8")
-    accesscode = requests.get("https://m-applications.cf/gak.php").content.decode("utf-8")
-    return f"<h1>در حال آماده سازی</h1><script>var a = document.createElement('a');a.href='https://m-applications.cf/download.php?version={ver}&k={accesscode}';a.click();</script>"
+    verurls = json.loads(requests.get("https://m-applications.cf/releases/version_urls.json").content.decode("utf-8"))
+    return f"<h1>در حال آماده سازی</h1><script>var a = document.createElement('a');a.href='{verurls[ver]}';a.click();</script>"
 
 @app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
