94c45b89860a2a2ccee9ddf04776757334116748
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 22:46:23 +0430

    One Step until Finish

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def download():
     ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content.decode("utf-8")
     accesscode = requests.get("https://m-applications.cf/gak.php").content.decode("utf-8")
-    return redirect(f"https://m-applications.cf/download.php?version={ver}&k={accesscode}")
+    return f"<h1>در حال آماده سازی</h1><script>var a = document.createElement('a');a.href='https://m-applications.cf/download.php?version={ver}&k={accesscode}';a.click();</script>"
 
 @app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
