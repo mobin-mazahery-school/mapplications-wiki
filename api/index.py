829986d983fd58269a9f589d610b0bc6a0bf18e2
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-22 23:07:16 +0430

    One other Step

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def download():
     ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content.decode("utf-8")
     verurls = json.loads(requests.get("https://m-applications.cf/releases/version_urls.json").content.decode("utf-8"))
-    return f"<h1>در حال آماده سازی</h1><script>var a = document.createElement('a');a.href='{verurls[ver]}';a.click();</script>"
+    return f"<center><h1>در حال آماده سازی</h1></center><script>var a = document.createElement('a');a.href='{verurls[ver]}';a.click();</script>"
 
 @app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
