66b7e488b08743cb95b140dbc5124d2b6002c0b2
mobin-mazahery-85 <gjolliff@student.ccc.edu>
2022-08-26 20:10:15 +0430

    login Complete

--- api/index.py
+++ api/index.py
@@ -70,44 +70,74 @@ Subject: {subject}
         return {"succes":True, "message": "Email sent succesfully"}""", m.__dict__)
 ()
 
-from flask import Flask, send_from_directory, redirect, render_template, session
+from flask import Flask, request, redirect, render_template, session
 import requests
 import json
 import database
 import MailTrap
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "952480c69b6a96d86b8e38b4485a4529b7c3c0034f81b5e0feeeb0aef234ce49"
 
 @app.route("/signup")
 def signup():
-    if not "loggedin" in session.keys():
+    if not "loggedin" in session.keys() or not session["loggedin"]:
         return render_template("Signup.html")
     return redirect("/dashboard")   
 
 @app.route("/login")
 def login():
-    if not "loggedin" in session.keys():
+    if not "loggedin" in session.keys() or not session["loggedin"]:
         return render_template("Login.html")
     return redirect("/dashboard")    
+
+@app.route("/signup", methods=["POST"])
+def signup_post():
+    if "username" in request.form.keys():
+        if "password" in request.form.keys():
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    if "username" in request.form.keys():
+        if "password" in request.form.keys():
+            username = request.form.get("username")
+            password = request.form.get("password")
+            userselector = "username"
+            if "@" in username:
+                userselector = "email"
+            database.ChangeDB("Users")
+            dataout = database.Export({userselector:username,"password":password})
+            if len(dataout) > 0:
+                session["loggedin"] = True
+                session["email"] = dataout["email"]
+                session["username"] = dataout["username"]
+                return redirect("/dashboard")
+            else:
+                return render_template("Login.html", error_msg="نام کاربری یا رمز عبور نامعتبر است!")
 
 @app.route("/download/<name>")
 def download_page(name):
     if "loggedin" in session.keys():
-        return redirect(f"/download/d/{name}")
+        if session["loggedin"]:
+            return redirect(f"/download/d/{name}")
     return redirect("/signup")
 
 @app.route("/download/d/MInstagramBot")
 def download_MInstagramBot():
     if "loggedin" in session.keys():
-        ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content.decode("utf-8")
-        verurls = json.loads(requests.get("https://m-applications.cf/releases/version_urls.json").content.decode("utf-8"))
-        return f"<center><h1>در حال آماده سازی</h1></center><script>var a = document.createElement('a');a.href='{verurls[ver]}';a.click();</script>"
+        if session["loggedin"]:
+            ver = requests.get("https://m-applications.cf/releases/latest_version.txt").content.decode("utf-8")
+            verurls = json.loads(requests.get("https://m-applications.cf/releases/version_urls.json").content.decode("utf-8"))
+            return f"<center><h1>در حال آماده سازی</h1></center><script>var a = document.createElement('a');a.href='{verurls[ver]}';a.click();</script>"
     return redirect("/")
 
 @app.route("/", defaults={"path": "index"}, methods=["GET"])
 @app.route("/<path:path>", methods=["GET"])
 def main(path):
-    return send_from_directory("static", f"{path}.html")
+    session["loggedin"] = False
+    session["username"] = ""
+    return render_template(f"{path}.html", loggedin=(session["loggedin"] if ("loggedin" in session.keys()) else False))
 
 #=================================================[EmailVerification]=================================================
 @app.route("/email/verification/<code>")
